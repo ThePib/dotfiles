59654fdbfbb370a165c1fc6dc3e666fc83f605d6
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2018-10-24 19:02:52 +0200

    Importation des fichiers de configuration

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+# fish configuration
+
+alias l='exa'
+alias ls='exa'
+alias ll='exa -lgh --git'
+alias la='exa -algh --git'
+alias otp='oathtool --totp -b (cat $HOME/.pd_otp_secret) | xclip -sel clip'
+alias vim='nvim'
+
+set -x EDITOR nvim
+set -x TERMINAL alacritty
+set -x PATH $HOME/.cargo/bin $PATH
+set -x VISUAL nvim
+
+set -g theme_color_scheme zenburn
+set -g theme_display_cmd_duration no
+set -g theme_display_date no
+
+if test -z $DISPLAY; and test (tty) = "/dev/tty1"
+	exec startx
+end

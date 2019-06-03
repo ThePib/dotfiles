10e095b81f178bf397a7acf045a04b17a1925383
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-06-03 22:46:14 +0200

    Update polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar laptop &
 polybar screen1 &
-polybar screen2 &
 
 echo "Bars launched..."

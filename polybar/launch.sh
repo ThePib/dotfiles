ed2c2ded25fe3898694787411936460b920ef7e5
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-06-03 22:41:29 +0200

    Polybar: automatically run a single bar on all screens

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar screen1 &
-
-echo "Bars launched..."
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar --reload current &
+    done
+else
+    polybar --reload current &
+fi

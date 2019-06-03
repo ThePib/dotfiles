c965422384ff127b0ad6c04dbce1e04bebc5f805
Théo Piboubès <734711+ThePib@users.noreply.github.com>
2019-06-03 22:45:54 +0200

    Add/update some environment variables

--- fish/config.fish
+++ fish/config.fish
@@ -8,8 +8,10 @@ alias otp='oathtool --totp -b (cat $HOME/.pd_otp_secret) | xclip -sel clip'
 alias vim='nvim'
 
 set -x EDITOR nvim
+set -x NPM_CONFIG_PREFIX $HOME/.node_modules
+set -x PATH $HOME/.node_modules/bin $HOME/.cargo/bin $PATH
+set -x TERM xterm-256color
 set -x TERMINAL kitty
-set -x PATH $HOME/.cargo/bin $PATH
 set -x VISUAL nvim
 
 set -g theme_color_scheme zenburn
